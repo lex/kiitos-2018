b12e2a0ed0300c4ed8c60d48e00c9cfe86b27b70
lex <lex@users.noreply.github.com>
2018-02-17 20:01:07 +0200

    enable http requests to insecure sources

--- src/services/observations.js
+++ src/services/observations.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
+import https from 'https';
 
 const baseUrl =
   process.env.NODE_ENV === 'production'
     ? 'http://kiitos-2018.herokuapp.com/'
     : 'http://127.0.0.1:5000/';
 
-const API = axios.create({ baseURL: baseUrl });
+const API = axios.create({
+  baseURL: baseUrl,
+  // too poor to pay heroku
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+});
 
 const getPoints = () =>
   API.get('observation-points/').then(response => response.data);
