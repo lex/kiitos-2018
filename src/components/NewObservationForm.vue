ab75b9152432d62a329dcd0453e551dd2e1483e2
lex <lex@users.noreply.github.com>
2018-02-21 11:37:58 +0200

    make sure the new temperature form gets a string as the placeholder

--- src/components/NewObservationForm.vue
+++ src/components/NewObservationForm.vue
@@ -1,14 +1,14 @@
 <template>
-    <div id="form">
-        <b-form @submit="onSubmit">
-            <b-form-group id="inputGroup" label="Add new temperature:" label-for="observation" :description="description">
-                <b-form-input id="observation" type="text" v-model="form.observation" required :placeholder="placeholder">
-                </b-form-input>
-            </b-form-group>
-            <b-button type="submit" variant="primary">Add</b-button>
-        </b-form>
-        <b-alert id="error" show variant="danger" v-if="error">{{ error }}</b-alert>
-    </div>
+  <div id="form">
+    <b-form @submit="onSubmit">
+      <b-form-group id="inputGroup" label="Add new temperature:" label-for="observation" :description="description">
+        <b-form-input id="observation" type="text" v-model="form.observation" required :placeholder="placeholder">
+        </b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Add</b-button>
+    </b-form>
+    <b-alert id="error" show variant="danger" v-if="error">{{ error }}</b-alert>
+  </div>
 </template>
 
 <script>
@@ -27,14 +27,21 @@ export default {
   computed: {
     placeholder() {
       const placeholderTemperature = 296.65;
+      let formattedTemperature = 0.0;
+
       switch (this.temperatureFormat) {
         case 'celsius':
-          return kelvinToCelsius(placeholderTemperature).toFixed(2);
+          formattedTemperature = kelvinToCelsius(placeholderTemperature);
+          break;
         case 'fahrenheit':
-          return kelvinToFahrenheit(placeholderTemperature).toFixed(2);
+          formattedTemperature = kelvinToFahrenheit(placeholderTemperature);
+          break;
         default:
-          return placeholderTemperature;
+          formattedTemperature = placeholderTemperature;
+          break;
       }
+
+      return `${formattedTemperature.toFixed(2)}`;
     },
     description() {
       return `In the selected unit (${unitForFormat(this.temperatureFormat)})`;
